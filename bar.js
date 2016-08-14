d4e9676cd4e92220b6fa158212c3e2e924e399c0
damian.przygodzki <damian.przygodzki@jazzy.pro>
2016-08-14 14:07:31 +0200

    Event fix #2

--- bar.js
+++ bar.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+document.onload = function() {
     var bar = document.createElement('div');
     document.body.appendChild(bar);
     bar.style.cssText = "width: 100%; font-family: sans-serif; position: fixed; bottom: 0; left: 0; background-color: rgba(0,0,0,.5); color: #fff; line-height: 40px; font-size: 13px; text-align: right;";
