a13e67fb21713aecacb531d2ae2c1e3785186299
damian.przygodzki <damian.przygodzki@jazzy.pro>
2016-08-14 14:10:12 +0200

    Event fix #3

--- bar.js
+++ bar.js
@@ -1,4 +1,4 @@
-document.onload = function() {
+(function() {
     var bar = document.createElement('div');
     document.body.appendChild(bar);
     bar.style.cssText = "width: 100%; font-family: sans-serif; position: fixed; bottom: 0; left: 0; background-color: rgba(0,0,0,.5); color: #fff; line-height: 40px; font-size: 13px; text-align: right;";
@@ -6,4 +6,4 @@ document.onload = function() {
     if(barConfig.gitUri){
         bar.innerHTML = '<a href="'+ barConfig.gitUri +'" style="color: #fff; text-decoration: none; margin-right: 20px;">Github</a>';
     }
-}
+}());
